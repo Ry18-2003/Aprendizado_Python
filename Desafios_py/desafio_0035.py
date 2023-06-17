d7b58e9fa3371169b5edf055618fcc94651b6b11
Ry18-2003 <richard.chrispe@hotmail.com>
2023-06-17 13:05:50 -0300

    Teste

---
+++ Desafios_py/desafio_0035.py
@@ -0,0 +1,29 @@
+from cores import letra_amarelo, letra_azul, letra_vermelho, reset
+
+amarelo = letra_amarelo
+azul = letra_azul
+vermelho = letra_vermelho
+
+a1 = int(input("Escreva um número: "))
+a2 = int(input(f"Usar qual base de converção.\n{amarelo}Binaria[0]\n{azul}Octal[1]\n{vermelho}Hexadecimal[2]{reset}\nEscolha: "))
+
+if a2 == 0:
+
+    b2 = ''
+
+    while a1 != 0:
+        
+        b1 = a1 % 2
+        a1 = a1 // 2
+        b2 = b2 + str(b1)
+        
+    print(b2[::-1])  # Comando bin pode ser usado para substituir essa parte matematica
+
+elif a2 == 1:
+    print(oct(a1[2:]))
+
+elif a2 == 2:
+    print(hex(a1[2:]))
+
+else:
+    print("Nenhum valor valido selecionado. Tenha um bom dia.")
---
+++ Desafios_py/desafio_0036.py
@@ -0,0 +1,11 @@
+# Ler dois valores e falar qual o maior
+
+a1 = float(input('Digite um valor: '))
+a2 = float(input('Digite um segundo valor: '))
+
+if a1 > a2:
+    print(f'Primeiro valor ({a1:.0f}) é maior que o segundo valor ({a2:.0f})')
+elif a2 > a1:
+    print(f'Segundo valor ({a2:.0f}) é maior que o primeiro ({a1:.0f})')
+else:
+    print(f'Não ha valor maior.')
